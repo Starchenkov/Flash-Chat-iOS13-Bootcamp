d5b9168ed573a8b87952979fef7be2d8016cd0a9
Starchenkov <Starchenkov@live.ru>
2021-02-16 10:50:17 +0700

    Final result

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
-    @IBAction func loginPressed(_ sender: UIButton) {
-    }
     
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error{
+                    let alert = UIAlertController(title: "Warning", message: "\(e.localizedDescription)", preferredStyle: UIAlertController.Style.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }else {
+                    //Navigate to the Chat ViewController
+                    self.performSegue(withIdentifier: K.loginSegue, sender: self)
+                }
+            }
+        }
+    }
 }
